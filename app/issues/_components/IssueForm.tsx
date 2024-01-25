0c2118c85ea1212909fe719fe1d5e7edb37a102a
Noman Syed <noman.syed@sastaticket.pk>
2024-01-25 15:28:21 +0500

    Update issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -28,7 +28,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data)
+      if (issue)
+        await axios.patch(`/api/issues/${issue.id}`, data)
+      else
+        await axios.post('/api/issues', data)
       router.push('/issues')
     } catch (error) {
       setSubmitting(false);
@@ -51,7 +54,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <Button disabled={isSubmitting}>
+          { issue ? 'Update issue' : 'Submit New Issue' }{' '}{isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   )
